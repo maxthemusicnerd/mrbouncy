fd8085a05741d41270094bc5511b8626527f4abe
Chioma <96903876+chiomaeme@users.noreply.github.com>
2025-11-15 13:24:10 -0700

    ball collision

--- scripts/bouncy_ball.gd
+++ scripts/bouncy_ball.gd
@@ -1,10 +1,13 @@
 extends RigidBody2D
 
 @export var gravity: int = 100
-
+var velocity = Vector2(250, 250)
 
 func _physics_process(delta: float) -> void:
 
 
-    velocity = move_toward()
-	pass
+	#velocity = move_toward()
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		var collision_point = collision_info.get_position()
+		velocity = velocity.bounce(collision_info.get_normal())
